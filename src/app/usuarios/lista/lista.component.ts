2d03521e6c3121247c0695a740f52969e50fdd4c
Francisco Manuel Cortes Hernandez <francisco.cortes@globant.com>
2020-07-31 18:04:42 -0500

    First commit-Create structure

---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: [],
+})
+export class ListaComponent implements OnInit {
+  usuarios: Usuario[] = [];
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.usuarioService.getusers().subscribe((users) => {
+      this.usuarios = users;
+      console.log(this.usuarios);
+    });
+  }
+}
